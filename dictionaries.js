14082d09ede291b7ed0891aa8e14eb5bc7af46f1
pincesCPU987 <75500175+pincesCPU987@users.noreply.github.com>
2022-10-27 11:41:44 -0700

    Update dictionaries.js

--- dictionaries.js
+++ dictionaries.js
@@ -9,65 +9,75 @@ class Dict {
 					opcode: 'addItem',
 					blockType: Scratch.BlockType.COMMAND,
 					text: 'add [VALUE] to dictionary [DICT] with key [KEY]',
-					VALUE: {
-						type: Scratch.ArgumentType.STRING,
-						defaultValue: 'thing'
-					},
-					DICT: {
-						type: Scratch.ArgumentType.STRING,
-						defaultValue: 'my dictionary'
-					},
-					KEY: {
-						type: Scratch.ArgumentType.STRING,
-						defaultValue: 'item 1'
+					arguments: {
+						VALUE: {
+							type: Scratch.ArgumentType.STRING,
+							defaultValue: 'thing'
+						},
+						DICT: {
+							type: Scratch.ArgumentType.STRING,
+							defaultValue: 'my dictionary'
+						},
+						KEY: {
+							type: Scratch.ArgumentType.STRING,
+							defaultValue: 'item 1'
+						}
 					}
 				},
 				{
 					opcode: 'createDict',
 					blockType: Scratch.BlockType.COMMAND,
 					text: 'create dictionary [DICT]',
-					DICT: {
-						type: Scratch.ArgumentType.STRING,
-						defaultValue: 'my dictionary'
+					arguments: {
+						DICT: {
+							type: Scratch.ArgumentType.STRING,
+							defaultValue: 'my dictionary'
+						}
 					}
 				},
 				{
 					opcode: 'replaceItem',
 					blockType: Scratch.BlockType.COMMAND,
 					text: 'replace item with key [KEY] from dictionary [DICT] with [VALUE]',
-					KEY: {
-						type: Scratch.ArgumentType.STRING,
-						defaultValue: 'item 1',
-					},
-					DICT: {
-						type: Scratch.ArgumentType.STRING,
-						defaultValue: 'my dictionary'
-					}
-					VALUE: {
-						type: Scratch.ArgumentType.STRING,
-						defaultValue: 'thing'
+					arguments: {
+						KEY: {
+							type: Scratch.ArgumentType.STRING,
+							defaultValue: 'item 1',
+						},
+						DICT: {
+							type: Scratch.ArgumentType.STRING,
+							defaultValue: 'my dictionary'
+						}
+						VALUE: {
+							type: Scratch.ArgumentType.STRING,
+							defaultValue: 'thing'
+						}
 					}
 				},
 				{
 					opcode: 'reportItem',
 					blockType: Scratch.BlockType.REPORTER,
 					text: 'item with key [KEY] from dictionary [DICT]',
-					KEY: {
-						type: Scratch.ArgumentType.STRING,
-						defaultValue: 'item 1'
-					},
-					DICT: {
-						type: Scratch.ArumentType.STRING,
-						defaultValue: 'my dictionary'
+					arguments: {
+						KEY: {
+							type: Scratch.ArgumentType.STRING,
+							defaultValue: 'item 1'
+						},
+						DICT: {
+							type: Scratch.ArumentType.STRING,
+							defaultValue: 'my dictionary'
+						}
 					}
 				}
 				{
 					opcode: 'isEmpty',
 					blockType: Scratch.BlockType.BOOLEAN,
 					text: 'dictionary [DICT] is empty?',
-					DICT: {
-						type: Scratch.ArgumentType.STRING,
-						defaultValue: 'my dictionary'
+					arguments: {
+						DICT: {
+							type: Scratch.ArgumentType.STRING,
+							defaultValue: 'my dictionary'
+						}
 					}
 				}
 			]
